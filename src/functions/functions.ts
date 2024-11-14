c4a76ab875b5988cf328081a9bb2111ec510a1b9
Saya Ozaki <project.mononeco@gmail.com>
2024-11-14 11:27:13 +0900

    implement Functions

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -1,4 +1,4 @@
-﻿/* global clearInterval, console, CustomFunctions, setInterval */
+﻿/* global clearInterval, CustomFunctions, setInterval */
 
 /**
  * Adds two numbers.
@@ -9,30 +9,6 @@
  */
 export function add(first: number, second: number): number {
   return first + second;
-}
-
-/**
- * Displays the current time once a second.
- * @customfunction
- * @param invocation Custom function handler
- */
-export function clock(invocation: CustomFunctions.StreamingInvocation<string>): void {
-  const timer = setInterval(() => {
-    const time = currentTime();
-    invocation.setResult(time);
-  }, 1000);
-
-  invocation.onCanceled = () => {
-    clearInterval(timer);
-  };
-}
-
-/**
- * Returns the current time.
- * @returns String with the current time formatted for the current locale.
- */
-export function currentTime(): string {
-  return new Date().toLocaleTimeString();
 }
 
 /**
@@ -54,13 +30,104 @@ export function increment(incrementBy: number, invocation: CustomFunctions.Strea
 }
 
 /**
- * Writes a message to console.log().
- * @customfunction LOG
- * @param message String to write.
- * @returns String to write.
+ * generate zebra pattern.
+ * @customfunction
+ * @param invocation Custom function handler
  */
-export function logMessage(message: string): string {
-  console.log(message);
+export function zebra(invocation: CustomFunctions.StreamingInvocation<number[][]>): void {
+  const gs = new GrayScotModel(0.04, 0.06, 100);
 
-  return message;
+  const timer = setInterval(() => {
+    gs.update();
+    invocation.setResult(gs.materialU);
+  }, 10);
+
+  invocation.onCanceled = () => {
+    clearInterval(timer);
+  };
+}
+
+class GrayScotModel {
+  feed: number;
+  kill: number;
+  materialU: number[][];
+  materialV: number[][];
+  spaceGridSize: number;
+
+  readonly Dx = 0.01;
+  readonly Dt = 1;
+  readonly Du = 2e-5;
+  readonly Dv = 1e-5;
+
+  constructor(feed: number, kill: number, spaceGridSize: number) {
+    this.feed = feed;
+    this.kill = kill;
+    this.spaceGridSize = spaceGridSize;
+    this.materialU = Array.from(new Array(spaceGridSize), () => new Array(spaceGridSize).fill(1));
+    this.materialV = Array.from(new Array(spaceGridSize), () => new Array(spaceGridSize).fill(0));
+
+    const fromRange = Math.floor(spaceGridSize / 2) - Math.floor(spaceGridSize / 20);
+    const toRange = Math.floor(spaceGridSize / 2) + Math.floor(spaceGridSize / 20);
+
+    for (let i = fromRange; i < toRange; i++) {
+      for (let j = fromRange; j < toRange; j++) {
+        this.materialU[i][j] = 0.5;
+        this.materialV[i][j] = 0.25;
+      }
+    }
+
+    for (let i = 0; i < spaceGridSize; i++) {
+      for (let j = 0; j < spaceGridSize; j++) {
+        this.materialU[i][j] += Math.random() * 0.1;
+        this.materialV[i][j] += Math.random() * 0.1;
+      }
+    }
+  }
+
+  update() {
+    const laplacianU: number[][] = Array.from(new Array(this.spaceGridSize), () => new Array(this.spaceGridSize));
+    const laplacianV: number[][] = Array.from(new Array(this.spaceGridSize), () => new Array(this.spaceGridSize));
+
+    const firstIndex = 0;
+    const lastIndex = this.spaceGridSize - 1;
+
+    for (let i = 0; i < this.spaceGridSize; i++) {
+      for (let j = 0; j < this.spaceGridSize; j++) {
+        const rollDownIndex = i === 0 ? lastIndex : i - 1;
+        const rollUpIndex = i === lastIndex ? firstIndex : i + 1;
+        const rollRightIndex = j === 0 ? lastIndex : j - 1;
+        const rollLeftIndex = j === lastIndex ? firstIndex : j + 1;
+
+        laplacianU[i][j] =
+          (this.materialU[rollDownIndex][j] +
+            this.materialU[rollUpIndex][j] +
+            this.materialU[i][rollRightIndex] +
+            this.materialU[i][rollLeftIndex] -
+            4 * this.materialU[i][j]) /
+          (this.Dx * this.Dx);
+        laplacianV[i][j] =
+          (this.materialV[rollDownIndex][j] +
+            this.materialV[rollUpIndex][j] +
+            this.materialV[i][rollRightIndex] +
+            this.materialV[i][rollLeftIndex] -
+            4 * this.materialV[i][j]) /
+          (this.Dx * this.Dx);
+      }
+    }
+
+    for (let i = 0; i < this.spaceGridSize; i++) {
+      for (let j = 0; j < this.spaceGridSize; j++) {
+        const dudt =
+          this.Du * laplacianU[i][j] -
+          this.materialU[i][j] * this.materialV[i][j] * this.materialV[i][j] +
+          this.feed * (1.0 - this.materialU[i][j]);
+        const dvdt =
+          this.Dv * laplacianV[i][j] +
+          this.materialU[i][j] * this.materialV[i][j] * this.materialV[i][j] -
+          (this.feed + this.kill) * this.materialV[i][j];
+        this.materialU[i][j] += this.Dt * dudt;
+        this.materialV[i][j] += this.Dt * dvdt;
+      }
+    }
+  }
 }
